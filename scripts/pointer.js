369ea601b6f34b89662e4ff908df88874cdc9d2d
sndrtkr <31322658+sndrtkr@users.noreply.github.com>
2020-09-28 15:09:46 +0200

    new design

--- scripts/pointer.js
+++ scripts/pointer.js
@@ -1,76 +1,182 @@
-/* 
-    pointer.js was created by OwL for use on websites, 
-     and can be found at https://seattleowl.com/pointer.
-*/
 
-const pointer = document.createElement("div")
-pointer.id = "pointer-dot"
-const ring = document.createElement("div")
-ring.id = "pointer-ring"
-document.body.insertBefore(pointer, document.body.children[0])
-document.body.insertBefore(ring, document.body.children[0])
 
-let mouseX = -100
-let mouseY = -100
-let ringX = -100
-let ringY = -100
-let isHover = false
-let mouseDown = false
-const init_pointer = (options) => {
+// cursor settings
 
-    window.onmousemove = (mouse) => {
-        mouseX = mouse.clientX
-        mouseY = mouse.clientY
+    var pointer = $(".pointer");
+
+    $(window).mousemove(function(e) {
+        pointer.css({
+            top: e.clientY - pointer.height() / 2,
+            left: e.clientX - pointer.width() / 2
+        });
+    });
+
+    $(window)
+        .mouseleave(function() {
+            pointer.css({
+                opacity: "0"
+            });
+        })
+        .mouseenter(function() {
+            pointer.css({
+                opacity: "1"
+            });
+        });
+
+//Scale cursor on link hover
+    $('a')
+        .mouseenter(function() {
+            pointer.css({
+                transform: "scale(3.2)"
+            });
+        })
+        .mouseleave(function() {
+            pointer.css({
+                transform: "scale(1)"
+            });
+        });
+
+//Scale cursor on soft cirque hover
+        // $("#softcirque")
+        // .mouseenter(function() {
+        //     pointer.css({
+        //         transform: "scale(3.2)"
+        //     });
+        // })
+        // .mouseleave(function() {
+        //     pointer.css({
+        //         transform: "scale(1)"
+        //     });
+        // });
+
+
+
+
+    $(window)
+        .mousedown(function() {
+            pointer.css({
+                transform: "scale(.2)"
+            });
+        })
+        .mouseup(function() {
+            pointer.css({
+                transform: "scale(1)"
+            });
+        });
+
+
+
+ // Softcirque follower
+
+// var mouseX = 0, mouseY = 0;
+// $(document).mousemove(function(e){
+//    mouseX = e.pageX;
+//    mouseY = e.pageY;
+// });
+
+// // cache the selector
+// var follower = $("#softcirque");
+// var xp = 0;
+// var yp = 0;
+
+// var loop = setInterval(function(){
+//     var width = $(window).width();
+//     mouseX = Math.max(mouseX, 20);
+//     mouseX = Math.min(mouseX, width-20);
+//     // change 12 to alter damping higher is slower
+//     xp += (mouseX - xp) / 12;
+//     follower.css({left:xp});  
+//     yp += (mouseY - yp) / 12;
+//     follower.css({top:yp});    
+// }, 30);
+
+
+// IMAGE SLIDESHOW
+
+class Slideshow {
+
+    constructor() {
+      this.initSlides();
+      this.initSlideshow();
     }
-
-    window.onmousedown = (mouse) => {
-        mouseDown = true
+  
+    // Set a `data-slide` index on each slide for easier slide control.
+    initSlides() {
+      this.container = $('[data-slideshow]');
+      this.slides = this.container.find('img');
+      this.slides.each((idx, slide) => $(slide).attr('data-slide', idx));
     }
-
-    window.onmouseup = (mouse) => {
-        mouseDown = false
+  
+    // Pseudo-preload images so the slideshow doesn't start before all the images
+    // are available.
+    initSlideshow() {
+      this.imagesLoaded = 0;
+      this.currentIndex = 0;
+      this.setNextSlide();
+      this.slides.each((idx, slide) => {
+        $('<img>').on('load', $.proxy(this.loadImage, this)).attr('src', $(slide).attr('src'));
+      });
     }
-
-    const trace = (a, b, n) => {
-        return (1 - n) * a + n * b;
+  
+    // When one image has loaded, check to see if all images have loaded, and if
+    // so, start the slideshow.
+    loadImage() {
+      this.imagesLoaded++;
+      if (this.imagesLoaded >= this.slides.length) { this.playSlideshow() }
     }
-    window["trace"] = trace
-
-    const getOption = (option) => {
-        let defaultObj = {
-            pointerColor: "#000",
-            ringSize: 0,
-            ringClickSize: (options["ringSize"] || 15) - 5,
-        }
-        if (options[option] == undefined) {
-            return defaultObj[option]
-        } else {
-            return options[option]
-        }
+  
+    // Start the slideshow.
+    playSlideshow() {
+      this.slideshow = window.setInterval(() => { this.performSlide() }, 4000);
     }
-
-    const render = () => {
-        ringX = trace(ringX, mouseX, 0.2)
-        ringY = trace(ringY, mouseY, 0.2)
-
-        if (document.querySelector(".p-action-click:hover")) {
-            pointer.style.borderColor = getOption("pointerColor")
-            isHover = true
-        } else {
-            pointer.style.borderColor = "black"
-            isHover = false
-        }
-        ring.style.borderColor = getOption("pointerColor")
-        if (mouseDown) {
-            ring.style.padding = getOption("ringClickSize") + "px"
-        } else {
-            ring.style.padding = getOption("ringSize") + "px"
-        }
-
-        pointer.style.transform = `translate(${mouseX}px, ${mouseY}px)`
-        ring.style.transform = `translate(${ringX - (mouseDown ? getOption("ringClickSize") : getOption("ringSize"))}px, ${ringY - (mouseDown ? getOption("ringClickSize") : getOption("ringSize"))}px)`
-
-        requestAnimationFrame(render)
+  
+    // 1. Previous slide is unset.
+    // 2. What was the next slide becomes the previous slide.
+    // 3. New index and appropriate next slide are set.
+    // 4. Fade out action.
+    performSlide() {
+      if (this.prevSlide) { this.prevSlide.removeClass('prev fade-out') }
+  
+      this.nextSlide.removeClass('next');
+      this.prevSlide = this.nextSlide;
+      this.prevSlide.addClass('prev');
+  
+      this.currentIndex++;
+      if (this.currentIndex >= this.slides.length) { this.currentIndex = 0 }
+  
+      this.setNextSlide();
+  
+      this.prevSlide.addClass('fade-out');
     }
-    requestAnimationFrame(render)
-}
+  
+    setNextSlide() {
+      this.nextSlide = this.container.find(`[data-slide="${this.currentIndex}"]`).first();
+      this.nextSlide.addClass('next');
+    }
+  
+  }
+  
+  $(document).ready(function() {
+    new Slideshow;
+  });
+  
+
+
+  //menu
+
+
+  $("#about").click(function() {
+    $("#aboutp").toggle();
+  });
+
+//   $("#about").click(function() {
+//     $("#about").css("textAlign","left");
+//   });
+
+  $("#play").click(function() {
+    $("#playp").toggle();
+    $("#gradient").toggle();
+  });
+
+
+  
